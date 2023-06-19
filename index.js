78d107c1ee48f5946733cbfe3d830e41eeda1c2e
Cédric BRASSEUR <50497186+cbrasseur@users.noreply.github.com>
2023-06-19 11:07:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const createIndexFile = () => {
     if (!fs.existsSync('build')) {
         fs.mkdirSync('build')
     }
-    fs.writeFileSync('build/index.html', 'Bonjour les .Net');
+    fs.writeFileSync('build/index.html', 'Bonjour les CDA');
 };
 
 createIndexFile();
