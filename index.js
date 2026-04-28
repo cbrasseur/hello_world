cb3e259999375f214aaabd3b4b51b29fd80df412
Cédric BRASSEUR <50497186+cbrasseur@users.noreply.github.com>
2026-04-28 16:35:01 +0200

    Update index.html greeting message

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const createIndexFile = () => {
     if (!fs.existsSync('build')) {
         fs.mkdirSync('build')
     }
-    fs.writeFileSync('build/index.html', 'Bonjour les DI');
+    fs.writeFileSync('build/index.html', 'Salut les ESI CYB 25-27 !');
 };
 
 createIndexFile();
